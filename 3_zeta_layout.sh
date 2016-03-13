1a9164daae9f3b002e781503e9af9867ae08c8be
darkness <darkness@hadoopmapr1.brewingintel.com>
2016-03-13 14:27:36 -0500

    fix all nodes

--- 3_zeta_layout.sh
+++ 3_zeta_layout.sh
@@ -280,7 +280,7 @@ EOL2
 # This is the stub version of this, more information can be added as needed
 
 # GET ALL NODES
-ALL_NODES=$(sudo maprcli node list -columns ip|cut -d" " -f1|grep -v "hostname")
+ALL_NODES=$(sudo maprcli node list -columns ip|cut -d" " -f1|grep -v "hostname"|tr "\n" " ")
 
 # Get 3 Master Nodes for Mesos Master and Marathon Master  
 MASTER_NODES=$(echo ${ALL_NODES}|cut -d" " -f1,2,3)
