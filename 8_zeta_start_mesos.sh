f89903bd9d4f8554b262bc903ebe07fef79b3f9b
darkness <darkness@hadoopmapr1.brewingintel.com>
2016-03-15 12:59:04 -0500

    Added notes on mesos cluster start

--- 8_zeta_start_mesos.sh
+++ 8_zeta_start_mesos.sh
@@ -6,3 +6,8 @@ CLUSTERNAME=$(ls /mapr)
 
 cd /home/zetaadm/zetaadmin
 ./startmesos.sh 1
+
+
+echo "Mesos should be started now.  At this point you should ssh in  and create a SOCKS proxy connection so you can see the UIs of various services"
+echo "Note: After you install mesos-dns, if you drop your SSH Connection and re-establish, and you use remote dns in firefox, you'll get name resolution!"
+
